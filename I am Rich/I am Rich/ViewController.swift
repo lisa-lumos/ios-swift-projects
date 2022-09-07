e24ba4cfa3f29df464bc145b3090f0e583224311
lisa-lumos <zjcty307@gmail.com>
2022-09-06 19:32:01 -0600

    I am Rich App

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Lisa J on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
